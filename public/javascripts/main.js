ab1c72cd02c51f5edd63ef0975e2faf6ca80b5c1
DeftSlyNinja <christopher_masson@hotmail.com>
2024-02-26 13:42:13 -0800

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,151 @@
+let GameArray = [];
+
+let GameObject = function (pID, pTitle, pGenre, pLink, pPriority) {
+    this.ID = generateID();
+    this.title = pTitle;
+    this.genre = pGenre;
+    this.link = pLink;
+    this.prio = pPriority;
+}
+
+GameArray.push(new GameObject(1, "Persona 3", "RPG", "https://store.steampowered.com/app/2161700/Persona_3_Reload/", 1));
+GameArray.push(new GameObject(2, "Call of Duty", "FPS", "https://store.steampowered.com/app/1938090/Call_of_Duty/", 2));
+GameArray.push(new GameObject(3, "XCOM", "Strategy", "https://store.steampowered.com/app/200510/XCOM_Enemy_Unknown/", 3));
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    createBacklog();
+
+    document.getElementById("addButton").addEventListener("click", function () {
+        GameArray.push(new GameObject((GameArray.length + 1), document.getElementById("title").value, selectedType, document.getElementById("link").value,
+            document.getElementById("priority").value));
+
+        alert(document.getElementById("title").value + " has been added to the backlog!")
+        document.getElementById("title").value = "";
+        document.getElementById("link").value = "";
+        document.getElementById("priority").value = "";
+
+        createBacklog();
+    })
+
+    $(document).bind("change", "#genre", function (event, ui) {
+        selectedType = document.getElementById("genre").value;
+    });
+
+    $(document).bind("change", "#selectFilter", function (event, ui) {
+        filteredGenre = document.getElementById("selectFilter").value;
+    });
+
+    document.getElementById("filterButton").addEventListener("click", createFilteredList);
+
+    document.getElementById("searchButton").addEventListener("click", searchBacklog);
+})
+
+// Functions Below
+
+function createBacklog() {
+    let table = document.getElementById("tableID");
+    table.innerHTML = "";
+    table.innerHTML = "<thead><th>ID</th><th>Title</th><th>Genre</th><th>Link</th><th>Priority</th></thead>"
+
+    let backlogUL = document.getElementById("backlogUL");
+    backlogUL.innerHTML = "";
+
+    GameArray.forEach(function (element,) {
+        const newRow = document.createElement("tr");
+        const tdID = document.createElement("td");
+        const tdTitle = document.createElement("td");
+        const tdGenre = document.createElement("td");
+        const tdLink = document.createElement("td");
+        const tdPrio = document.createElement("td");
+
+        tdID.textContent = element.ID;
+        tdPrio.textContent = element.prio;
+        tdTitle.textContent = element.title;
+        tdGenre.textContent = element.genre;
+        tdLink.textContent = element.link;
+
+        newRow.appendChild(tdID);
+        newRow.appendChild(tdTitle);
+        newRow.appendChild(tdGenre);
+        newRow.appendChild(tdLink);
+        newRow.appendChild(tdPrio);
+        table.appendChild(newRow);
+
+        let li = document.createElement("li");
+        let link = document.createElement("a");
+        link.href = element.link;
+        link.target = "_blank";
+        link.className = "ui-btn ui-btn-icon-right ui-icon-carat-r";
+        link.innerHTML = element.ID + " -  " + element.title + " (" + element.genre + ")";
+        li.appendChild(link);
+        backlogUL.appendChild(li);
+    })
+
+
+    let rows = table.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        let currentRow = table.rows[i];
+        let createClickHandler =
+            function (row) {
+                return function () {
+                    let cell = row.getElementsByTagName("td")[0];
+                    let whichID = cell.innerHTML;
+                    // alert(whichID);
+                    openYouTube(whichID);
+                }
+            }
+        currentRow.onclick = createClickHandler(currentRow);
+    }
+}
+
+function openYouTube(which) {
+    for (i = 0; i < GameArray.length; i++) {
+        if (which == GameArray[i].ID) {
+            window.open(GameArray[i].link);
+        }
+    }
+}
+
+function createFilteredList() {
+    let filteredUL = document.getElementById("filteredUL");
+    filteredUL.innerHTML = "";
+
+    GameArray.forEach(function (game,) {
+        if (game.genre == document.getElementById("selectFilter").value) {
+            let li = document.createElement("li");
+            let link = document.createElement("a");
+            link.href = game.link;
+            link.target = "_blank";
+            link.className = "ui-btn ui-btn-icon-right ui-icon-carat-r";
+            link.innerHTML = game.ID + "-  " + game.title + " (" + game.genre + ")";
+            li.appendChild(link);
+            filteredUL.appendChild(li);
+        }
+    })
+
+}
+
+function searchBacklog() {
+    let searchEntry = document.getElementById("searchEntry");
+
+    GameArray.forEach(function (game,) {
+        if (game.title.toLowerCase() == searchEntry.value.toLowerCase()) {
+            let li = document.createElement("li");
+            let link = document.createElement("a");
+            link.href = game.link;
+            link.target = "_blank";
+            link.className = "ui-btn ui-btn-icon-right ui-icon-carat-r";
+            link.innerHTML = game.ID + "-  " + game.title + " (" + game.genre + ")";
+            li.appendChild(link);
+            filteredUL.appendChild(li);
+        }
+    })
+
+    searchEntry.innerHTML = "";
+}
+
+function generateID() {
+    let newID = Math.random().toString(16).slice(5);
+    return newID;
+}
---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+
+let ServerGameArray = [];
+
+let GameObject = function (pID, pTitle, pGenre, pLink, pPriority) {
+    this.ID = generateID();
+    this.title = pTitle;
+    this.genre = pGenre;
+    this.link = pLink;
+    this.prio = pPriority;
+}
+
+ServerGameArray.push(new GameObject(1, "Persona 3", "RPG", "https://store.steampowered.com/app/2161700/Persona_3_Reload/", 1));
+ServerGameArray.push(new GameObject(2, "Call of Duty", "FPS", "https://store.steampowered.com/app/1938090/Call_of_Duty/", 2));
+ServerGameArray.push(new GameObject(3, "XCOM", "Strategy", "https://store.steampowered.com/app/200510/XCOM_Enemy_Unknown/", 3));
+
+console.log(ServerGameArray);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('index.html');
+});
+
+/* GET Backlog data */
+router.get('/getAllGames', function(req, res) {
+  res.status(200).json(ServerGameArray);
+});
+
+/* Add one new movie */
+router.post('/AddGame', function(req, res) {
+  const newGame = req.body;
+  ServerGameArray.push(newGame);
+  res.status(200).json(newGame);
+});
+
+// add route for delete
+router.delete('/DeleteMovie/:ID', (req, res) => {
+  const delID = req.params.ID;
+  let found = false;
+  let pointer = GetArrayPointer(delID);
+  if(pointer == -1){ // if did not find movie in array
+  console.log("not found");
+  return res.status(500).json({
+  status: "error - no such ID"
+  });
+  }
+  else { // if did find the movie
+  serverArray.splice(pointer, 1); // remove 1 element at index
+  res.send('Movie with ID: ' + delID + ' deleted!');
+  }
+});
+
+// cycles thru the array to find the array element with a matching ID
+function GetArrayPointer(localID) {
+  for (let i = 0; i < serverArray.length; i++) {
+  if (localID === serverArray[i].ID) {
+  return i;
+  }
+  }
+  return -1; // flag to say did not find a movie
+}
+
+module.exports = router;
+
+function generateID() {
+  let newID = Math.random().toString(16).slice(5);
+  return newID;
+}
